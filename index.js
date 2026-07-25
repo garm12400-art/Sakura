3b668fbbd249aa0c9b284d514e3b26a5884c7e23
garm12400-art <garm12400@gmail.com>
2026-07-25 18:23:20 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,37 +4,20 @@
  */
 
 const { spawn } = require("child_process");
-const express = require("express");
 const log = require("./logger/log.js");
-
-const app = express();
-const PORT = process.env.PORT || 1000;
-
-// Host / Uptime Checkers er jonno simple route
-app.get("/", (req, res) => {
-    res.send("Bot is alive and running!");
-});
-
-// Port open implementation to pass Port Scanning
-app.listen(PORT, () => {
-    log.info("INDEX", `Express server is listening on port: ${PORT}`);
-});
 
 function startProject() {
     const child = spawn("node", ["Sakura.js"], {
         cwd: __dirname,
         stdio: "inherit",
         shell: true,
-        env: { ...process.env, PORT: PORT }
+        env: { ...process.env, PORT: process.env.PORT || 1000 } // Port 1000 default
     });
 
     child.on("close", (code) => {
-        if (code === 2) {
+        if (code == 2) {
             log.info("Restarting Project...");
             startProject();
-        } else if (code !== 0) {
-            log.err("INDEX", `Process exited with code ${code}. Restarting in 3 seconds...`);
-            setTimeout(startProject, 3000);
         }
     });
 
